47d9569a1f07b091725a4e477363e754b269860b
Ramita <ramitaarora23@gmail.com>
2024-09-06 10:49:31 -0700

    initial homepage setup finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,40 @@
-import inventory from './data.json';
+import { useState, useEffect } from 'react';
+import data from './data.json';
 
 export default function App() {
+  const [inventory, setInventory] = useState([])
+
+  useEffect(() => {
+    if (data.length) {
+      // Sort data by things unsold here
+      setInventory(data);
+    }
+  }, [data])
 
   return (
     <div>
       <header>
         <h1>Ishita's Ceramics</h1>
+        <nav>
+          <ul>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
       </header>
       <main>
-
+        <div className="inventory-container">
+          {inventory.map((item, index) => (
+            <div className="inventory-item" key={index}>
+              <img src={item.image} alt={item.name} />
+              <div className="inventory-details">
+                <h2>{item.name}</h2>
+                <p>${item.price}</p>
+                <p>{item.sold ? 'Sold!' : null}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </main>
       <footer>
         <p>© 2024</p>
