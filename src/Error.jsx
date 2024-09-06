2c30fb2f0c98af93c86c241ca98dd3e735d4dcc8
Ramita <ramitaarora23@gmail.com>
2024-09-06 09:29:33 -0700

    installed react router

---
+++ src/Error.jsx
@@ -0,0 +1,7 @@
+export default function Error() {
+    return (
+        <div>
+            <p>Error Page</p>
+        </div>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Error from './Error.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "*",
+    element: <Error />
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
